898ab0a7c268ed2139f6b0e21a8b7ed6653ac05a
María Naranjo Almeida <maria2000naranjo@gmail.com>
2021-04-28 16:37:27 +0100

    First commit. Added location function

--- App.js
+++ App.js
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'; //API de localización
 
 export default function App() {
+  const [location, setLocation] = useState(null); //Geolocalización
+  const [errorMsg, setErrorMsg] = useState(null); //Error
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync(); //Pide al usuario permiso, mientras la aplicación está en primer plano. Devuelve promesa
+      if (status !== 'granted') { //Si no se lo ha dado, produce un error
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log(location);
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location); //Convertimos la location a string
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>{text}</Text>
     </View>
   );
 }
@@ -19,3 +43,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+/*
+Recordar para siguiente versión:
+Fórmula de una circunferencia: (x - h)^2 + (y - k)^2 = r^2
+*/
+
+/*
+Para acceder a uno solo de los parámetros de location (p.e: latitud):
+location.coords.latitude
+
+*/
